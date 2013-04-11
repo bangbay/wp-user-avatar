930213dc99ad8289deb9f81ddfd83a6c18d16393
Bangbay Siboliban <bangbay@mulu.me>
2013-04-11 09:31:55 -0700

    Move jQuery to register_script.

--- wp-user-avatar.php
+++ wp-user-avatar.php
@@ -211,7 +211,8 @@ if(!class_exists('wp_user_avatar')){
       if(function_exists('wp_enqueue_media')){
         wp_enqueue_media();
       } else {
-        wp_enqueue_script('jquery-1.7', 'http'.$ssl.'://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js');
+        wp_deregister_script('jquery');
+        wp_register_script('jquery', 'http'.$ssl.'://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js', false, '1.7.1');
         wp_enqueue_script('media-upload');
         wp_enqueue_script('thickbox');
         wp_enqueue_style('thickbox');
